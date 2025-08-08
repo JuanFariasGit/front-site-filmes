1ec35a7ca3c8eaf731ad8489a58d9c5e6896f3d5
Juan <juanfarias580@gmail.com>
2025-08-08 12:25:02 -0300

    chore: adicionando variavel de ambiente para a url da api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios"
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: __API_URL__,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
